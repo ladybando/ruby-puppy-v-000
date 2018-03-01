47db82db0f0a816acef83fff28423c34dd1d67ac
Ayana Bando <ayanaflax@yahoo.com>
2018-03-01 17:09:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,17 @@
 
 class Dog
   attr_accessor:name
-  attr_reader:clear_all
   @@all = []
   def initialize(name)
     @name=name
     @@all << self
   end
+  def self.clear_all
+     @@all.clear
+   end
   def self.all
     @@all.each do|puppy|
       puts puppy.name
   end
-  def self.clear_all
-     @@all.clear
-   end
   end
 end
